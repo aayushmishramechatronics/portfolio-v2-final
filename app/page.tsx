7f858f25317d8d4021bbb65bdf543c3615a2f959
Aayush Mishra <manofganges@gmail.com>
2025-07-16 17:52:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,37 +18,34 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col">
-        <TypographyH1>Hi, I&apos;m WhyAsh5114 👋</TypographyH1>
-        <span className="font-semibold mt-2">Mumbai, India 📌</span>
+        <TypographyH1>I&apos;m Aayush Mishra 👋</TypographyH1>
+        <span className="font-semibold mt-2">MIT Manipal, Udupi 📌</span>
         <TypographyP>
-          I&apos;m a passionate full-stack web developer, who likes to build
-          quality open-source web apps for everyone!
+          Undergraduate at MIT Manipal - Pursuing B.Tech in Mechatronics.
         </TypographyP>
         <TypographyP>
-          My primary tech stack is TypeScript, React, NextJS, TailwindCSS,
-          PostgreSQL, and Prisma. I also dabble in other stuff like Python and
-          Svelte.
+          I&apos;m a Full-Stack Developer and an Embedded Roboticist, I build and break projects - be it Hardware or Software related.
         </TypographyP>
 
         <div className="grid md:grid-cols-2 gap-1.5 my-8">
           <TechStack />
-          <Link href="https://github.com/WhyAsh5114">
+          <Link href="https://github.com/aayushmishramechatronics">
             <Card>
               <CardHeader className="flex-row justify-between">
                 <div className="flex flex-col gap-2">
                   <CardTitle>GitHub</CardTitle>
-                  <CardDescription>WhyAsh5114</CardDescription>
+                  <CardDescription>aayushmishramechatronics</CardDescription>
                 </div>
                 <GitHub className="w-8 h-8" />
               </CardHeader>
             </Card>
           </Link>
-          <Link href="https://www.linkedin.com/in/whyash5114/">
+          <Link href="https://www.linkedin.com/in/aayush-anil-mishra/">
             <Card>
               <CardHeader className="flex-row justify-between">
                 <div className="flex flex-col gap-2">
                   <CardTitle>LinkedIn</CardTitle>
-                  <CardDescription>whyash5114</CardDescription>
+                  <CardDescription>Aayush Anil Mishra</CardDescription>
                 </div>
                 <LinkedIn className="w-8 h-8" />
               </CardHeader>
